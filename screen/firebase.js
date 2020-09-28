b548232e713a7eacae13466a00699b7e5abcf023
Alexa fato <alexafato@MacBook-Pro-Alexa.local>
2020-09-28 16:56:40 +0300

    1

---
+++ screen/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyDHd5n6kq9C4qKbyt7kgcd6jeCuJlzsAiw",
+  authDomain: "reminder-app-diana.firebaseapp.com",
+  databaseURL: "https://reminder-app-diana.firebaseio.com",
+  projectId: "reminder-app-diana",
+  storageBucket: "reminder-app-diana.appspot.com",
+  messagingSenderId: "877353721217",
+  appId: "1:877353721217:web:6df016cae38a5546a15c80",
+  measurementId: "G-6CLDV5LQG5"
+});
+
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
